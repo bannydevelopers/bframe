05260ea0268071d2e297034050aeb9baaf98c15c
abigairi <abigailjonathan150@gmail.com>
2023-10-25 13:39:08 +0300

    staff complete

--- Application/system/plugins/Human_resources/modules/staff.php
+++ Application/system/plugins/Human_resources/modules/staff.php
@@ -2,7 +2,23 @@
 
 $config = storage::get_data('system_config')->db_configs;
 $db = db::get_connection($config);
-
+if(isset($_POST['delete_staff'])){
+    $db->delete('staff')->where(['user_reference'=>intval($_POST['delete_staff'])])->commit();
+    if(!$db->error()) $db->delete('user_accounts')->where(['user_id'=>intval($_POST['delete_staff'])])->commit();
+    if(!$db->error()){
+        $msg = [
+            'status'=>'success',
+            'message'=>'Staff deleted successful'
+        ];
+    }
+    else{
+        $msg = [
+            'status'=>'error',
+            'message'=>$db->error()['message']
+        ];
+    }
+    die(json_encode( ["response"=>$msg]));
+}
 
 $staff = $db->select('staff')
               ->join('user_accounts','user_id=user_reference')
