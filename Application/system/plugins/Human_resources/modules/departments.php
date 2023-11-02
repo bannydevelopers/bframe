ca6ff1096859ea73a3238ed953f5b38132195632
abigairi <abigailjonathan150@gmail.com>
2023-11-02 17:11:00 +0300

    payroll

--- Application/system/plugins/Human_resources/modules/departments.php
+++ Application/system/plugins/Human_resources/modules/departments.php
@@ -1,27 +1,10 @@
-<?php 
+<?php
 
 $config = storage::get_data('system_config')->db_configs;
 $db = db::get_connection($config);
 
-human_resources::save_department();
-if(isset($_POST['delete_department'])){
-    $k = $db->delete('departments')->where(['dept_id'=>$_POST['dept_id']])->commit();
-    if(!$db->error()) $msg = 'ok';
-    else $msg = $db->error()['message'];
-    die($msg);
-}
-$departments = $db->select('departments')
-                  ->join('faculties','faculty_id=dept_faculty')
-                  ->order_by('faculty_id', 'desc')
-                  ->fetchAll();
+$departments = $db->select('departments')->fetchAll();
 
-$d_tree = [];
-foreach($departments as $d){
-    if(!isset($d_tree[$d['faculty_name']])) $d_tree[$d['faculty_name']] = [];
-    $d_tree[$d['faculty_name']][] = $d;
-}
-$faculties = $db->select('faculties')->fetchAll();
-$body = '';
 ob_start();
 include __DIR__.'/html/departments.html';
 $body = ob_get_clean();
--- Application/system/plugins/Human_resources/modules/payroll.php
+++ Application/system/plugins/Human_resources/modules/payroll.php
@@ -1,59 +1,42 @@
 <?php 
-
 $config = storage::get_data('system_config')->db_configs;
 $db = db::get_connection($config);
-if(isset($_POST['p_slips'])){
-    if($helper->user_can('can_add_payroll')){
-        $slist = implode(',', array_values($_POST['p_slips']));
-        $slips = $db->select('salary_slip', "salary_slip.*, CONCAT_WS(' ',first_name, middle_name, last_name) AS full_name")
-                    ->join('user','salary_slip.employee=user.user_id')
-                    ->where("slip_id IN ({$slist})")
-                    ->order_by('slip_id', 'desc')->fetchAll();
-        $data = [
-            'payment_slips'=>json_encode($slips), 
-            'payroll_name'=>"{$_POST['payroll_month']}, {$_POST['payroll_year']}", 
-            'created_by'=>helper::init()->get_session_user('user_id'),
-            'create_date'=>date('Y-m-d H:i:s')
-            
-        ];
-        $k = $db->insert('payroll', $data);
-        //var_dump($db->error());
-    
-        if(!$db->error() && $k) {
-            $msg = 'Payroll added successful';
-            $ok = true;
-        }
-        else $msg = 'Error adding payroll';
+if(isset($_POST['employee'])){
+    $ok = 'error';    
     }
-    else{
-        $msg = 'Permission denied!';
-        $ok = false;
-    }
-   //var_dump($db->error());
-}
-
-if(isset($_POST['ajax_delete_payroll'])){
-    $db->delete('payroll')->where(['payroll_id'=>intval($_POST['ajax_delete_payroll'])])->commit();
-    if(!$db->error()) die('ok');
-    else die('fail');
-}
-
-else{
-    $msg = 'Permission denied!';
-    $ok = false;
-}
-$payroll = $db->select('payroll')
+    if(isset($_POST['p_slips'])){
+            $slist = implode(',', array_values($_POST['p_slips']));
+            $slips = $db->select('salary_slip', "salary_slip.*, full_name")
+                       ->join('user_accounts','salary_slip.employee=user_accounts.user_id')
+                       ->where("slip_id IN ({$slist})")
+                       ->order_by('slip_id', 'desc')->fetchAll();
+            $addData = [  
+                'payment_slips'=>json_encode($slips), 
+                'created_by'=>user::init()->get_session_user('user_id'),
+                'payroll_name'=>"{$_POST['payroll_month']}, {$_POST['payroll_year']}", 
+            ];
+            $k = $db->insert('payroll', $addData);
+           //var_dump($db->error());
+         if(!$db->error() && $k) {
+                $msg = 'Payroll added successful';
+                $ok  =  'success';
+            }
+            else $msg = $db->error()['message'];
+         if(isset($_POST['ajax_request'])){
+            die(json_encode(['status'=>$ok, 'message'=>$msg]));
+           }
+     }
+  $payroll = $db->select('payroll')
               ->order_by('payroll_id', 'desc')->fetchAll();
-
-    //var_dump($db->error());
-$employee = $db->select('user')
+        var_dump($db->error());
+  $employee = $db->select('user')
                ->where(1)
                ->fetchAll();
 
-$slips = $db->select('salary_slip')
-            ->join('user','salary_slip.employee=user.user_id')
+  $slips = $db->select('salary_slip')
+            ->join('user_accounts','salary_slip.employee=user_accounts.user_id')
             ->order_by('slip_id', 'desc')->fetchAll();
-
+//var_dump($db->error(), $slips);
 ob_start();
 include __DIR__.'/html/payroll.html';
 $body = ob_get_clean();
