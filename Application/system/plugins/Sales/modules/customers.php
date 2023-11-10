d0ddf96c7b3a7cef2c63806effdb0a5ed0104137
abigairi <abigailjonathan150@gmail.com>
2023-11-10 16:27:31 +0300

    product2

---
+++ Application/system/plugins/Sales/modules/customers.php
@@ -0,0 +1,79 @@
+<?php 
+    if(isset($_POST['delete_customer'])){
+        $db->delete('customer')->where(['user_reference'=>intval($_POST['delete_customer'])])->commit();
+        if(!$db->error()) $db->delete('user_accounts')->where(['user_id'=>intval($_POST['delete_customer'])])->commit();
+        if(!$db->error()){
+            $msg = [
+                'status'=>'success',
+                'message'=>'Customer deleted successful'
+            ];
+        }
+        else{
+            $msg = [
+                'status'=>'error',
+                'message'=>$db->error()['message']
+            ];
+        }
+        die(json_encode( ["response"=>$msg])); 
+        foreach($lines as $k=>$line) {
+            if(empty($line)) continue;
+            $lines[$k] = explode(',', $line); // convert string to array
+            if(count($lines[0]) != 15) die('Invalid file uploaded');
+            if($k == 0) continue;
+            $lines[$k][11] = date('Y-m-d', strtotime($lines[$k][11]));
+            $lines[$k][13] = date('Y-m-d', strtotime($lines[$k][13]));
+            $lines[$k][14] = date('Y-m-d', strtotime($lines[$k][14]));
+            $system_role[] = trim($lines[$k][3]);
+            $bank[] = trim($lines[$k][4]);
+            $designation[] = trim($lines[$k][8]);
+            $work_location[] = trim($lines[$k][9]);
+            $department[] = trim($lines[$k][10]);
+        }
+        $msg = 'Unknown error occured, probably file format not recognized';
+        $status = 'error';
+        $last_line = [];
+        if(!$db->error()){
+            $qry = 'BEGIN;'.PHP_EOL;
+            foreach($lines as $k => $line){
+                if($k == 0 or empty($line[0])) continue;
+
+                $last_line = $line;
+                $role = array_search($line[3], array_column($system_roles, 'role_id'));
+                $role_id = $system_roles[$role]['role_id'];
+
+                $pass = system::create_hash($line[1]);
+                $qry .= "INSERT INTO user_accounts (full_name, email, phone, passcode) VALUES ('{$line[0]}','{$line[1]}','{$line[2]}','{$pass}');".PHP_EOL;
+
+                $qry .= "INSERT INTO customer (user_reference, bank, bank_account_number, registration_number, residence_address,designation, work_location, department, date_employed, employment_length, employment_last_renewal, employment_end_date) VALUES (LAST_INSERT_ID(), '{$line[4]}','{$line[5]}','{$line[6]}','{$line[7]}','{$designation_id}','{$branch_id}','{$dept_id}','{$line[11]}','{$line[12]}','{$line[13]}','{$line[14]}');".PHP_EOL;
+            }
+            $qry .= 'COMMIT;';
+
+            $db->query($qry);
+            $chk = $db->select('customer', 'user_reference')
+                    ->where("user_reference IN (SELECT user_id FROM user_accounts user_id WHERE email ='{$last_line[1]}')")
+                    ->fetch();
+
+            if(!$db->error() && $chk) {
+                $msg = 'Customer import complete';
+                $status = 'success';
+            }
+            else $msg = 'Import failed';
+        }
+        die(json_encode(['message'=>$msg, 'status'=>$status]));
+    }
+    //var_dump($me);
+    if($me['work_location'] == $moduleconfig->headquarters_branch) {
+        $whr = 1;
+    }
+    else{
+        $whr = ['work_location'=>$me['work_location']];
+    }
+    $sortedCustomer = [];
+    foreach($customer as $st){
+        if(!isset($sortedCustomer[$st['designation_name']])) $sortedCustomer[$st['designation_name']] = [];
+        $sortedCustomer[$st['designation_name']][] = $st;
+    }
+    $body = '';
+    include __DIR__.'/html/customers.html';
+    $body = ob_get_clean();
+    $return = ['title'=>' ','body'=>$body];
--- Application/system/plugins/Sales/modules/products.php
+++ Application/system/plugins/Sales/modules/products.php
@@ -3,6 +3,9 @@ $me = human_resources::get_staff();
 if($me){
     $config = storage::get_data('system_config')->db_configs;
     $db = db::get_connection($config);
+    $productCategory = $db->select('product_category', 'category_id, category_name')
+                        ->where(1)
+                        ->fetchAll();
 
     $columns = "product.*, IFNULL(category_name, 'General') as category_name, IFNULL(branch_name, 'Headquarters') as branch_name";
     $Products = $db->select('product', $columns)
@@ -10,13 +13,20 @@ if($me){
                         ->join('branches', 'branch_id=owner_branch', 'LEFT')
                         ->where(1)
                         ->fetchAll();
+    $pic_root = 'Application/storage/uploads/products/product_thumb_';
     $sortedProduct = [];
     foreach($Products as $prod){
         if(!isset($sortedProduct[$prod['branch_name']])) $sortedProduct[$prod['branch_name']] = [];
         if(!isset($sortedProduct[$prod['branch_name']][$prod['category_name']])){
             $sortedProduct[$prod['branch_name']][$prod['category_name']] = [];
         }
-        $sortedProduct[$prod['branch_name']][$prod['category_name']] = $prod;
+        $root = '/storage/uploads/products/product_thumb_';
+        if(is_readable(realpath(__DIR__.'/../../../../')."{$root}{$prod['product_id']}.jpg")) 
+            $prod['product_image'] = "Application/storage/uploads/products/product_thumb_{$prod['product_id']}.jpg";
+        else
+            $prod['product_image'] = 'Application/storage/uploads/products/product_thumb_0.jpg';
+
+        $sortedProducts[$prod['branch_name']][$prod['category_name']][] = $prod;
     }
     //var_dump('<pre>',$sortedProduct);die;
     ob_start();
