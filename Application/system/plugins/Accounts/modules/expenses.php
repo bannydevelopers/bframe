79c5f6bcb5c318d147e93840cb366cb4bf6a4816
abigairi <abigailjonathan150@gmail.com>
2023-11-30 18:47:40 +0300

    expenses

---
+++ Application/system/plugins/Accounts/modules/expenses.php
@@ -0,0 +1,91 @@
+<?php 
+$me = human_resources::get_staff();
+if($me){
+    $hq = human_resources::get_headquarters_branch();
+    $config = storage::get_data('system_config')->db_configs;
+    $db = db::get_connection($config);
+    $status = 'error';
+    if(isset($_POST['expenses_date'])){
+        $data = [
+            'expenses_date'=>intval($_POST['expenses_date']),
+            'expenses_number_item'=>addslashes($_POST['expenses_number_item']),
+            'expenses_description'=>addslashes($_POST['expenses_description']),
+            'expenses_amount'=>addslashes($_POST['expenses_amount']),
+            'purchased_by'=>addslashes($_POST['purchased_by']),
+            'approved_by'=>addslashes($_POST['approved_by']),
+            'owner_branch'=>intval($me['work_location'])
+        ];
+        if(isset($_POST['expenses_id'])){
+            $k = intval($_POST['expenses_id']);
+            $db->update('expenses', $data)->where(['expenses_id'=>$k])->commit();
+        }
+        else{
+            $chk = $db->select('expenses', 'expenses_date')
+                      ->where(['expenses_date'=>$data['expenses_date'], 'owner_branch'=>$data['owner_branch']])
+                      ->limit(1)
+                      ->fetchAll();
+
+            if(!$chk) $k = $db->insert('expenses', $data);
+            else $k = 0;
+        }
+        if(isset($_FILES['expenses_image']) && is_readable($_FILES['expenses_image']['tmp_name']) && $k){
+            $dir = realpath(__DIR__.'/../../../../storage/uploads/expensess/');
+            $source = $_FILES['expenses_image']['tmp_name'];//expenses_thumb_0.jpg
+            $bp = system::upload_image($source, "{$dir}/expenses_{$k}.jpg", ['width'=>600, 'height'=>400]);
+
+            file_put_contents("{$dir}/tmp.jpg", file_get_contents($bp));
+            $thumb = system::upload_image("{$dir}/tmp.jpg", "{$dir}/expenses_thumb_{$k}.jpg", ['width'=>200, 'height'=>170]);
+        }
+        if(!$db->error() && $k) {
+            $msg = 'Saved successful';
+            $status = 'success';
+        }
+        else{
+            $err = $db->error();
+            $msg = $k == 0 ? 'expenses exists' : $err['message'];
+
+        }
+        if(isset($_POST['ajax_request'])) die(json_encode(['status'=>$status, 'message'=>$msg]));
+    }
+    if($me['work_location'] == human_resources::get_headquarters_branch()) {
+        $whr = 1;
+    }
+    else{
+        $whr = ['owner_branch'=>$me['work_location']];
+    }
+    $expensesCategory = $db->select('expenses_category', 'category_id, category_name')
+                        ->where($whr)
+                        ->fetchAll();
+
+    if($me['work_location'] == $hq) $whr = 1;
+    else $whr = ['owner_branch'=>$me['work_location']];
+
+    $columns = "expenses.*, IFNULL(category_name, 'General') as category_name, IFNULL(branch_name, 'Headquarters') as branch_name";
+    $expenses = $db->select('expenses', $columns)
+                        ->join('user_accounts','user_id=approved_by')
+                        ->join('expenses_category', 'category_id=expenses_category', 'LEFT')
+                        ->join('branches', 'branch_id=owner_branch', 'LEFT')
+                        ->where($whr)
+                        ->fetchAll();
+
+    $employees = $db->select('staff')
+                ->join('user_accounts','user_id=user_reference')
+                ->where($whr)
+                ->order_by('staff_id', 'desc')
+                ->fetchAll();
+
+    $pic_root = 'Application/storage/uploads/expenses/expenses_thumb_';
+    $sortedExpenses = [];
+    foreach($expenses as $prod){
+        if(!isset($sortedExpenses[$prod['branch_name']])) $sortedExpenses[$prod['branch_name']] = [];
+        if(!isset($sortedExpenses[$prod['branch_name']][$prod['category_name']])){
+            $sortedExpenses[$prod['branch_name']][$prod['category_name']] = [];
+        }
+        $sortedExpenses[$prod['branch_name']][$prod['category_name']][] = $prod;
+    }
+    //var_dump('<pre>', $sortedExpenses);die;
+    ob_start();
+    include __DIR__.'/html/expenses.html';
+    $body = ob_get_clean();
+    $return = ['title'=>' ','body'=>$body];
+}
--- Application/system/plugins/Communication/communication_class.php
+++ Application/system/plugins/Communication/communication_class.php
@@ -1,8 +1,5 @@
 <?php
-class accounts{
-    public $name = 'eTours';
-    public $version = 1.0;
-    public $desc = 'Plugin dedicated for complete tours management as a drop in';
+class Communication{
 
     private static $instance = null;
     
@@ -12,11 +9,11 @@ class accounts{
 
     public static function init(){
         // Hook to the system
-        system::add_event_listener('exec_end', 'accounts::load_page', $_SERVER['REQUEST_URI']);
+        system::add_event_listener('exec_end', 'communication::load_page', $_SERVER['REQUEST_URI']);
         // Hook to admin dashboard
-        system::add_event_listener('admin_plugin_load', 'accounts::load_admin_dashboard');
+        system::add_event_listener('admin_plugin_load', 'communication::load_admin_dashboard');
         // Respond to service requests
-        system::add_event_listener('add_bank', 'accounts::service_add_bank');
+        system::add_event_listener('add_bank', 'communication::service_add_bank');
     }
     public static function load_admin_dashboard($args){
         if(strtolower($args[0]) == __CLASS__) {
@@ -37,28 +34,6 @@ class accounts{
         $db = db::get_connection($conf->db_configs);
         
         $page = $registry->page;
-        if(!intval($page['page_id'])) return;
-
-        if($page['page_extras'] && is_string($page['page_extras'])) $extras = json_decode($page['page_extras']);
-        else $extras = [];
-        $page['page_extras'] = (array)$extras;
-
-        $obj = new static();
-        $bbcodes = template::find_bbcode($page['page_content']);
-        if(is_array($bbcodes)){
-            $supported = [''];
-            $searches = $replacement = [];
-            foreach($bbcodes as $bbc){
-                $bparts = explode('/', trim($bbc,' {$}'));
-                if(in_array($bparts[0], $supported)) {
-                    $searches[] = $bbc;
-                    $replacement[] = $obj->{$bparts[0]}($bparts);
-                }
-            }
-            
-            $page['page_content'] = str_replace($searches, $replacement, $page['page_content']);
-            storage::init()->page = $page;
-        }
     }
     public static function service_add_bank(){
         return '<form>Adding bank<button>Send</button></form>';
