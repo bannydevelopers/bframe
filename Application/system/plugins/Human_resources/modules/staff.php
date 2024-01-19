c3f247b9d1267b0d54db61363c34222c22dde0da
Baraka Mghumba <wizdontz@gmail.com>
2024-01-19 13:26:05 +0300

    export na import

--- Application/system/plugins/Human_resources/modules/staff.php
+++ Application/system/plugins/Human_resources/modules/staff.php
@@ -25,6 +25,7 @@ if($me){
         $fh = file_get_contents($_FILES['staff_list']['tmp_name']); // get contents of uploaded file
         //$lines = explode(PHP_EOL,$fh); //split lines
         $lines = preg_split('/\n|\r/', $fh, -1, PREG_SPLIT_NO_EMPTY); // split lines securely
+
         $system_role = $bank = $designation = $work_location = $department = [];
         //0-full_name, 1-email, 2-phone,3-system_role,4-bank,5-bank_account_number,6-registration_number,7-residence_address,
         //8-designation,9-work_location,10-department,11-date_employed,12-employment_length,13-employment_last_renewal,14employment_end_date
@@ -72,13 +73,13 @@ if($me){
                 $qry .= "INSERT INTO user_accounts (full_name, email, phone, system_role, passcode) VALUES ('{$line[0]}','{$line[1]}','{$line[2]}','{$role_id}','{$pass}');".PHP_EOL;
                 
                 $bnk = array_search($line[4], array_column($banks, 'bank_id'));
-                $bank_id = $banks[$bnk]['bank_id'];
+                $bank_id = @$banks[$bnk]['bank_id'];
                 
                 $desn = array_search($line[4], array_column($designations, 'designation_id'));
                 $designation_id = $designations[$desn]['designation_id'];
                 
                 $wl = array_search($line[4], array_column($work_locations, 'branch_id'));
-                $branch_id = $work_locations[$wl]['branch_id'];
+                $branch_id = @$work_locations[$wl]['branch_id'];
                 if($me['work_location'] != $moduleconfig->headquarters_branch) $branch_id = $me['work_location'];
                 
                 $dept = array_search($line[4], array_column($departments, 'dept_id'));
@@ -104,6 +105,24 @@ if($me){
     //var_dump($me);
     if($me['work_location'] == human_resources::get_headquarters_branch()) {
         $whr = 1;
+    }
+    if(isset($_POST['export_staff'])){
+        $export = $db->select('staff')
+                    ->join('user_accounts','user_id=user_reference')
+                    ->join('roles', 'system_role=role_id', 'left')
+                    ->join('designations', 'designation=designation_id', 'left')
+                    ->join('departments', 'department=dept_id', 'left')
+                    ->join('branches', 'work_location=branch_id', 'left')
+                    ->join('banks', 'bank=bank_id', 'left')
+                    ->where($whr)
+                    ->order_by('staff_id', 'desc')
+                    ->fetchAll();
+
+        $csv = "full_name, email, phone, system_role, bank, bank_account_number, registration_number, residence_address, designation, work_location, department, date_employed, employment_length, employment_last_renewal, employment_end_date\n";
+        foreach($export as $ex){
+            $csv .= "{$ex['full_name']},{$ex['email']},{$ex['phone']},{$ex['role_name']}, {$ex['bank_name']}, {$ex['bank_account_number']}, {$ex['registration_number']}, {$ex['residence_address']}, {$ex['designation_name']}, {$ex['branch_name']}, {$ex['dept_name']}, {$ex['date_employed']}, {$ex['employment_length']}, {$ex['employment_last_renewal']}, {$ex['employment_end_date']}\n";
+        }
+        die($csv);
     }
     else{
         $whr = ['work_location'=>$me['work_location']];
