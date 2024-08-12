a0236bc3778d166ea82f91dbada72e1e1b6ccbbc
abigairi <abigailjonathan150@gmail.com>
2024-08-12 10:55:50 +0300

    all

--- Application/system/plugins/Sales/modules/service_charge.php
+++ Application/system/plugins/Sales/modules/service_charge.php
@@ -18,35 +18,46 @@ if(isset($_POST['created_date'])){
         'created_by'=>addslashes($me['work_location'])
     ];
     $purch_items_qry = [];
-    $purch_id = $db->insert('service', $purch_data);
+    $purch_id = $db->insert('service_charge', $purch_data);
     if($purch_id && !$db->error()){
         foreach($_POST['item_name'] as $k=>$p){
-            $purch_items_qry[] = "('{$p}', {$purch_id}, {$_POST['quantity'][$k]}, '{$_POST['description'][$k]}')";
+            $purch_items_qry[] = "('{$p}', {$purch_id}, {$_POST['quantity'][$k]}, '{$_POST['description'][$k]}', {$_POST['price'][$k]})";
         }
         $purch_items_qry = implode(',', $purch_items_qry);
-        $db->query("INSERT INTO service_items (service_item_name, service_item_reference, service_item_quantity, service_item_description) VALUES {$purch_items_qry}");
+        $db->query("INSERT INTO service_charge_item (service_item_name, service_item_reference, service_item_quantity, service_item_description, service_item_price) VALUES {$purch_items_qry}");
         if($db->error()){
-            $db->delete('service')->where(['service_id'=>$purch_id])->commit();
+            $db->delete('service_charge')->where(['service_id'=>$purch_id])->commit();
         }
     }
-    //var_dump($db->error());
-    if($db->error()) $msg = $db->error()['message'];
-    else $msg = 'Saved successful';
-    if(isset($_POST['ajax_request'])) die($msg);
+    var_dump($db->error());
+$error = $db->error();
+
+if ($error) {
+    $msg = isset($error['message']) ? $error['message'] : 'An error occurred';
+} else {
+    $msg = 'Saved successfully';
 }
-if(isset($_POST['approve_service'])){
-    $db->update('service', ['approved_by'=>$me['user_reference']])
-        ->where(['service_id'=>intval($_POST['approve_service'])])
+
+if (isset($_POST['ajax_request'])) die($msg);
+}
+
+if (isset($_POST['approve_service'])) {
+    $db->update('service', ['approved_by' => $me['user_reference']])
+        ->where(['service_id' => intval($_POST['approve_service'])])
         ->commit();
 
-    if($db->error()) $msg = $db->error()['message'];
-    else $msg = 'Approved successful';
+    $error = $db->error();
+    if ($error) {
+        $msg = isset($error['message']) ? $error['message'] : 'An error occurred';
+    } else {
+        $msg = 'Approved successfully';
+    }
     die($msg);
 }
-if(isset($_POST['local_service_order'])){
+/*if(isset($_POST['service_charge'])){
     $data = [
         'reference_service'=>intval($_POST['reference_service']),
-        'local_service_order'=>addslashes($_POST['local_service_order']),
+        'service_charge'=>addslashes($_POST['service_charge']),
         'tax_service_remarks'=>addslashes($_POST['tax_service_remarks']),
         'recorded_by'=>user::init()->get_session_user('user_id')
     ];
@@ -59,7 +70,7 @@ if(isset($_POST['local_service_order'])){
         $msg = $db->error()['message'];
     }
     if(isset($_POST['ajax_request'])) die($msg);
-}
+}*/
 if(isset($_POST['qty'])){
     /*
         //INSERT INTO table_users (cod_user, date, user_rol, cod_office) VALUES
@@ -70,29 +81,29 @@ if(isset($_POST['qty'])){
         //cod_user=VALUES(cod_user), date=VALUES(date)
     */
     $tmp = [];
-    $lst = $db->select('service_items','service_item_id')
+    $lst = $db->select('service_charge_item','service_item_id')
               ->where('service_item_id')
               ->in(array_keys($_POST['qty']))
               ->fetchAll();
     foreach($_POST['qty'] as $id=>$qty){
         if(array_search($id, array_column($lst, 'service_item_id')) === false) $idx = 'NULL';
         else $idx = intval($id);
-        $tmp[] = "({$idx}, '{$_POST['item_name'][$id]}', '{$_POST['service']}', $qty, '{$_POST['description'][$id]}')";
+        $tmp[] = "({$idx}, '{$_POST['name'][$id]}', '{$_POST['service_id']}', $qty, '{$_POST['desc'][$id]}', '{$_POST['price'][$id]}')";
     }
+
     $tmp = implode(',', $tmp);
-    $qry = "INSERT INTO service_items (service_item_id, service_item_name, service_item_reference, service_item_quantity, service_item_description) VALUES "
-            . " {$tmp} ON DUPLICATE KEY UPDATE service_item_name = VALUES(service_item_name), service_item_description=VALUES(service_item_description), service_item_quantity=VALUES(service_item_quantity)";
+    $qry = "INSERT INTO service_charge_item (service_item_id, service_item_name, service_item_reference, service_item_quantity, service_item_description, service_item_price) VALUES "
+            . " {$tmp} ON DUPLICATE KEY UPDATE service_item_name = VALUES(service_item_name), service_item_description=VALUES(service_item_description), service_item_quantity=VALUES(service_item_quantity), service_item_price=VALUES(service_item_price)";
             
-    //var_dump($tmp);die;
     $db->query($qry);
-    //var_dump($db->error());
-    if($db->error()) die('Saving failed');
+    var_dump($db->error());
+    if($db->error()) die($db->error()['message']);
     else die('Saved successful');
 }
 if(isset($_POST['delete_service'])){
-    $db->delete('service_items')->where(['service_item_reference'=>intval($_POST['delete_service'])])->commit();
+    $db->delete('service_charge_item')->where(['service_item_reference'=>intval($_POST['delete_service'])])->commit();
     if(!$db->error()){
-        $db->delete('service')->where(['service_id'=>intval($_POST['delete_service'])])->commit();
+        $db->delete('service_charge')->where(['service_id'=>intval($_POST['delete_service'])])->commit();
         if($db->error()) $msg = $db->error()['message'];
         else $msg = 'service deleted successful!';
     }
@@ -103,7 +114,7 @@ if(isset($_POST['delete_service'])){
 }
 if(isset($_POST['delete_service_item'])){
     $idx = intval($_POST['delete_service_item']);
-    $db->delete('service_items')->where(['service_item_id'=>$idx])->commit();
+    $db->delete('service_charge_item')->where(['service_item_id'=>$idx])->commit();
     if($db->error()) $msg = $db->error()['message'];
     else $msg = 'ok';
     die($msg);
@@ -118,20 +129,20 @@ if($me['work_location'] == human_resources::get_headquarters_branch()) {
     $whr = 1;
 }
 else{
-    $whr = "service.owner_branch={$me['work_location']}";
+    $whr = "service_charge.owner_branch={$me['work_location']}";
 }
 $items_q = "(
                 SELECT JSON_ARRAYAGG(
                     JSON_OBJECT(
-                        'id', service_item_id, 'item_name', service_item_name, 'service', service_item_reference, 'description', service_item_description, 'qty', service_item_quantity
+                        'id', service_item_id, 'name', service_item_name, 'qty', service_item_quantity, 'desc', service_item_description, 'price', service_item_price
                     )
                 ) 
-                FROM service_items WHERE service_item_reference=service_id
+                FROM service_charge_item WHERE service_item_reference=service_id
             ) AS service_items";
 
-$qry = "service.*, approver.full_name as approved, branches.branch_name, user_accounts.full_name, {$items_q}, customer.*";
+$qry = "service_charge.*, approver.full_name as approved, branches.branch_name, user_accounts.full_name, {$items_q}, customer.*";
 
-$service = $db->select('service', $qry)
+$service = $db->select('service_charge', $qry)
                 ->join('branches','branch_id=owner_branch')
                 ->join('user_accounts', 'user_id=created_by')
                 ->join('user_accounts as approver', 'approver.user_id=approved_by', 'LEFT')
@@ -140,7 +151,6 @@ $service = $db->select('service', $qry)
                 ->order_by('service_id', 'desc')
                 ->fetchAll();   
 
-var_dump($db->error());
 $customer = $db ->select('customer','customer_id, customer_name')
                 ->where($whr)
                 ->fetchAll();
@@ -152,12 +162,8 @@ foreach($service as $prod){
     $sortedservice[$prod['branch_name']][] = $prod;
 }
 
-$default = [
-    'branch_name' => 'Default Branch Name',  // Example default value, modify as needed
-    'branch_profile' => 'Default Branch Profile'  // Example default value, modify as needed
-];
-
 $company = [];
+$default = []; // Initialize $default here
 $company[] = storage::get_data('system_config')->company_profile;
 $branches = $db->select('branches', 'branch_id,branch_profile')->where($whr)->fetchAll();
 foreach($branches as $b){
